884ad90b813b67a4ede8a67daa9905790e14b141
chskv1 <119333782+chskv1@users.noreply.github.com>
2024-03-30 18:38:19 +0100

    update bo poprzedni coś nie zadział

--- scripts/register.php
+++ scripts/register.php
@@ -1,1 +1,24 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['mail'];
+    $password = $_POST['password'];
+    $repassword = $_POST['repeat_password'];
+
+    if ($password !== $repassword) {
+
+    }
+    $connection = mysqli_connect();
+    if (!$connection) {
+        die('Connection error' . mysqli_connect_error());
+    }
+    $hashPass = password_hash($password, PASSWORD_DEFAULT);
+    $query = ""; //pamietaj zrobić querry
+    if (mysqli_query($connection, $query)) {
+        //header("Location: ")
+        exit();
+    } else {
+        echo 'register error' . mysqli_error($connection);
+    }
+    mysqli_close($connection);
+}
+?>
